7e7f8b6c8ce3b2d75456ccaa6c1545685d13a2c9
smallzoo <1010138814@qq.com>
2016-03-15 14:12:28 +0800

    update adapter

--- src/com/qst/fly/adapter/PictureSelectListAdapter.java
+++ src/com/qst/fly/adapter/PictureSelectListAdapter.java
@@ -2,9 +2,12 @@ package com.qst.fly.adapter;
 
 import java.util.List;
 
+import com.bumptech.glide.Glide;
+import com.qst.fly.R;
 import com.qst.fly.entity.Picture;
 
 import android.content.Context;
+import android.widget.ImageView;
 
 /**
 * @author smallzoo
@@ -13,14 +16,16 @@ import android.content.Context;
 * 类说明
 */
 public class PictureSelectListAdapter extends CommonAdapter<Picture> {
-
+	private Context context;
 	public PictureSelectListAdapter(Context context, List<Picture> mDatas, int itemLayoutId) {
 		super(context, mDatas, itemLayoutId);
+		this.context = context;
 	}
 
 	@Override
 	public void convert(ViewHolder helper, Picture item, int position) {
-		
+		ImageView imageView = helper.getView(R.id.img_item_pic);
+//		Glide.with(this.context).load(item.picturePath).into(imageView);
 	}
 
 }
